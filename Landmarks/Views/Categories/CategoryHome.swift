57aafde9159eb156f034563d32e2c6c434871538
Max Yoo <mazinggom@hotmail.com>
2025-04-20 22:52:39 +0900

    [dev] Add a category view

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by gandalgom on 4/20/25.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationSplitView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        } detail: {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+#Preview {
+    CategoryHome()
+}
